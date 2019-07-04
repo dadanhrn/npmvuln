67058ce44aef6ceeb10730988c005391c0e9bf6a
dadanhrn <dadanhrn@gmail.com>
2019-07-04 21:58:17 +0700

    Stat preprocessor

---
+++ src/main/scala/npmvuln/jobs/StatReadyDfBuilder.scala
@@ -0,0 +1,28 @@
+package npmvuln.jobs
+
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.expressions.UserDefinedFunction
+import org.apache.spark.sql.functions.{col, floor, max, min, udf}
+import org.threeten.extra.Interval
+import java.sql.Timestamp
+
+object StatReadyDfBuilder {
+  val getDurationInDay: UserDefinedFunction = udf[Int, Timestamp, Timestamp]((since, to) => {
+    Interval.of(since.toInstant, to.toInstant).toDuration.toDays.toInt
+  })
+
+  def build(resultDf: DataFrame): DataFrame = {
+
+    // Source dataframe
+    resultDf
+
+      // Group aggregation by vulnerability ID and package they're on
+      .groupBy("Id", "Package")
+
+      // Get earliest and latest date of vulnerability occurence in each package
+      .agg(min("Since").as("Since"), max("To").as("To"))
+
+      // Calculate difference between latest and earliest occurence
+      .withColumn("Duration", floor(getDurationInDay(col("Since"), col("To")) / 30))
+  }
+}
