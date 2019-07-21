fc10447972942ed5d5fe1327884b35fc0bc6680e
dadanhrn <dadanhrn@gmail.com>
2019-07-21 16:14:53 +0700

    Fix censor date

--- src/main/scala/npmvuln/helpers/constants/package.scala
+++ src/main/scala/npmvuln/helpers/constants/package.scala
@@ -5,5 +5,5 @@ import java.util.Date
 
 package object constants {
   // Censor date: 2017-11-10 00:00:00 UTC
-  val CENSOR_DATE: Instant = new Date(117,10,11,-17,0,0).toInstant
+  val CENSOR_DATE: Instant = new Date(117, 10, 4, -17, 0, 0).toInstant
 }
