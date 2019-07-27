94ab49fecb9eed70b1f9a7ba47b5405ea6e6c0e8
dadanhrn <dadanhrn@gmail.com>
2019-07-27 10:02:14 +0700

    Statready ungroup by duration

--- src/main/scala/npmvuln/jobs/StatReadyDfBuilder.scala
+++ src/main/scala/npmvuln/jobs/StatReadyDfBuilder.scala
@@ -13,10 +13,13 @@ object StatReadyDfBuilder {
     resultDf
 
       // Group aggregation by vulnerability ID and package they're on
-      .groupBy("Id", "Project", "Level", "Duration")
+      .groupBy("Id", "Project", "Level")
 
       // Get earliest and latest date of vulnerability occurence in each package
       .agg(min("StartDate").as("StartDate"), max("EndDate").as("EndDate"))
+
+      // Recalculate Duration
+      .withColumn("Duration", floor(datediff(col("EndDate"), col("StartDate")) / 30))
 
       // Get censor status (1 for observed, 0 for censored)
       .withColumn("Uncensored", when(col("EndDate") < Timestamp.from(CENSOR_DATE), 1).otherwise(0))
